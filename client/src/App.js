b746f837268ca0cd945c70d9d94fbdf5f50e2d60
paarth17032000 <paarth17032000@gmail.com>
2021-10-11 01:53:43 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-yellow-500 text-xl">JSS-Planner</div>
+    </div>
+  );
+}
+
+export default App;
