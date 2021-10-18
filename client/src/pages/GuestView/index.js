d90cfa23646217c1fce49c1abdde83ad2ec1da74
paarth17032000 <paarth17032000@gmail.com>
2021-10-18 12:21:58 +0530

    shruti lelo #2

--- client/src/pages/GuestView/index.js
+++ client/src/pages/GuestView/index.js
@@ -69,7 +69,8 @@ export default function TimeTable() {
     //           [{}, {}, {}, {}, {}, {}, {}]
     //         ]
     //     }
-    // )
+    // )\
+    console.log("cl",classes)
     return (
         <div className="min-h-screen bg-background">
 
@@ -156,14 +157,14 @@ export default function TimeTable() {
                             </Listbox>
                         </div>
                     </div>
-
+{console.log(classes)}
                     <div className="flex flex-col">
-                        <div className="font-regular font-12 text-shadow">Department</div>
+                        <div className="font-regular font-12 text-shadow">CLasses</div>
                         <div className="w-50">
                             <Listbox value={selectedClass} onChange={setSelectedClass}>
                                 <div className="relative mt-1">
                                     <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-md shadow-sm cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
-                                        <span className="block truncate font-semi-bold font-12 text-secondary">{selectedClass.name}</span>
+                                        <span className="block truncate font-semi-bold font-12 text-secondary">{selectedClass.code}</span>
                                         <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                                             <BiChevronDown
                                                 className="w-5 h-5 text-gray-400"
@@ -193,126 +194,9 @@ export default function TimeTable() {
                                                                 className={`${selectedClass ? 'font-medium hover:bg-hover-bg' : 'font-normal'
                                                                     } block truncate font-semi-bold font-12 text-secondary hover:bg-hover-bg cursor-pointer`}
                                                             >
-                                                                {person.name}
+                                                                {person.code}
                                                             </span>
                                                             {selectedClass ? (
-                                                                <span
-                                                                    className={`${active ? 'text-amber-600' : 'text-amber-600'
-                                                                        }
-                                                            absolute inset-y-0 left-0 flex items-center pl-3`}
-                                                                >
-                                                                    <BsCheck className="w-5 h-5" aria-hidden="true" />
-                                                                </span>
-                                                            ) : null}
-                                                        </>
-                                                    )}
-                                                </Listbox.Option>
-                                            ))}
-                                        </Listbox.Options>
-                                    </Transition>
-                                </div>
-                            </Listbox>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <div className="font-regular font-12 text-shadow">Subjects</div>
-                        <div className="w-50">
-                            <Listbox value={selectedClass} onChange={setSelectedClass}>
-                                <div className="relative mt-1">
-                                    <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-md shadow-sm cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
-                                        <span className="block truncate font-semi-bold font-12 text-secondary">{selectedClass.name}</span>
-                                        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                            <BiChevronDown
-                                                className="w-5 h-5 text-gray-400"
-                                                aria-hidden="true"
-                                            />
-                                        </span>
-                                    </Listbox.Button>
-                                    <Transition
-                                        as={Fragment}
-                                        leave="transition ease-in duration-100"
-                                        leaveFrom="opacity-100"
-                                        leaveTo="opacity-0"
-                                    >
-                                        <Listbox.Options className="absolute z-50 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                            {subjects?.map((person, personIdx) => (
-                                                <Listbox.Option
-                                                    key={personIdx}
-                                                    className={({ active }) =>
-                                                        `${active ? 'text-amber-900 bg-amber-100 hover:bg-hover-bg' : 'text-gray-900'}
-                                                    cursor-default select-none relative py-2 pl-10 pr-4 hover:bg-hover-bg`
-                                                    }
-                                                    value={person}
-                                                >
-                                                    {({ selectedClass, active }) => (
-                                                        <>
-                                                            <span
-                                                                className={`${selectedClass ? 'font-medium hover:bg-hover-bg' : 'font-normal'
-                                                                    } block truncate font-semi-bold font-12 text-secondary hover:bg-hover-bg cursor-pointer`}
-                                                            >
-                                                                {person.name}
-                                                            </span>
-                                                            {selectedClass ? (
-                                                                <span
-                                                                    className={`${active ? 'text-amber-600' : 'text-amber-600'
-                                                                        }
-                                                            absolute inset-y-0 left-0 flex items-center pl-3`}
-                                                                >
-                                                                    <BsCheck className="w-5 h-5" aria-hidden="true" />
-                                                                </span>
-                                                            ) : null}
-                                                        </>
-                                                    )}
-                                                </Listbox.Option>
-                                            ))}
-                                        </Listbox.Options>
-                                    </Transition>
-                                </div>
-                            </Listbox>
-                        </div>
-                        <button>Get CLass</button>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <div className="font-regular font-12 text-shadow">Semester</div>
-                        <div className="w-50">
-                            <Listbox value={selectedSemester} onChange={setSelectedSemester}>
-                                <div className="relative mt-1">
-                                    <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-md shadow-sm cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
-                                        <span className="block truncate font-semi-bold font-12 text-secondary">{selectedSemester.name}</span>
-                                        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                            <BiChevronDown
-                                                className="w-5 h-5 text-gray-400"
-                                                aria-hidden="true"
-                                            />
-                                        </span>
-                                    </Listbox.Button>
-                                    <Transition
-                                        as={Fragment}
-                                        leave="transition ease-in duration-100"
-                                        leaveFrom="opacity-100"
-                                        leaveTo="opacity-0"
-                                    >
-                                        <Listbox.Options className="absolute z-50 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                            {semester.map((person, personIdx) => (
-                                                <Listbox.Option
-                                                    key={personIdx}
-                                                    className={({ active }) =>
-                                                        `${active ? 'text-amber-900 bg-amber-100 hover:bg-hover-bg' : 'text-gray-900'}
-                                                    cursor-default select-none relative py-2 pl-10 pr-4 hover:bg-hover-bg`
-                                                    }
-                                                    value={person}
-                                                >
-                                                    {({ selectedSemester, active }) => (
-                                                        <>
-                                                            <span
-                                                                className={`${selectedSemester ? 'font-medium hover:bg-hover-bg' : 'font-normal'
-                                                                    } block truncate font-semi-bold font-12 text-secondary hover:bg-hover-bg cursor-pointer`}
-                                                            >
-                                                                {person.name}
-                                                            </span>
-                                                            {selectedSemester ? (
                                                                 <span
                                                                     className={`${active ? 'text-amber-600' : 'text-amber-600'
                                                                         }
